a798a81b8a132f678b28b2cb02540b8650f50b29
Maria <chesp.design@gmail.com>
2024-06-03 10:10:52 -0300

    fixtures e massa de dados

--- cypress/e2e/0-loja-ebac/login.cy.js
+++ cypress/e2e/0-loja-ebac/login.cy.js
@@ -1,11 +1,33 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
+
+
 describe('Funcionalidade: login', ()=>{
-    it ('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    })
+
+    it ('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('chesp.teste@teste.com')
-        cy.get('#password').type('teste@123')
+        cy.get('#password').type('teste123@')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, chesp.teste (não é chesp.teste? Sair)')
 
-    })
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, chesp.teste (não é chesp.teste? Sair)')
+    });
+
+    it('Deve fazer login com sucesso - Usando Fixture', () => {
+      cy.fixture('perfil').then( dados => {
+      cy.get('#username').type(dados.usuario)
+      cy.get('#password').type(dados.senha)
+      cy.get('.woocommerce-form > .button').click()
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, chesp.teste (não é chesp.teste? Sair)')
+      })
+    });
 })
